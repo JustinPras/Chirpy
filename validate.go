9f71bd3cf5668f2f98f65c123888e674c25b7f2d
JustinPras <justinponpras@gmail.com>
2025-05-07 20:24:42 +1000

    feat: censorship for chirps

--- validate.go
+++ validate.go
@@ -3,14 +3,17 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
+	"slices"
 )
 
 func (cfg *apiConfig) handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
+
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,7 +30,25 @@ func (cfg *apiConfig) handlerChirpsValidate(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	
+	censoredChirp := censorProfanity(params.Body)
+
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: censoredChirp,
 	})
+}
+
+func censorProfanity(chirp string) string {
+	profanity := []string{"kerfuffle", "sharbert", "fornax"}
+	const censor = "****"
+
+	words := strings.Split(chirp, " ")
+	for i, word := range(words) {
+		if slices.Contains(profanity, strings.ToLower(word)) {
+			words[i] = censor
+		}
+	}
+
+	censoredChirp := strings.Join(words, " ")
+	return censoredChirp
 }
