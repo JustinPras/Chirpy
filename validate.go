8369a010fe6365ab499b9045839acbfea5a526c0
JustinPras <justinponpras@gmail.com>
2025-05-07 19:43:32 +1000

    refactor: fix json code for chirp validation

--- validate.go
+++ validate.go
@@ -2,68 +2,32 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
-func (cfg *apiConfig) handlerValidate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	type chirpParams struct {
+func (cfg *apiConfig) handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
 		Body string `json:"body"`
 	}
-
-	type badReq struct {
-		Error string `json:"error"`
-	}
-
-	type goodReq struct {
+	type returnVals struct {
 		Valid bool `json:"valid"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
-	chirp := chirpParams{}
-	err := decoder.Decode(&chirp)
+	params := parameters{}
+	err := decoder.Decode(&params)
 	if err != nil {
-		respBody := badReq{
-			Error: "Something went wrong",
-		}
-		data, err := json.Marshal(respBody)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(data)
-		return
-	}
-	
-	if len(chirp.Body) > 140 {
-		respBody := badReq{
-			Error: "Chirp is too long",
-		}
-		data, err := json.Marshal(respBody)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(data)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
 		return
 	}
 
-	goodR := goodReq{
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{
 		Valid: true,
-	}
-
-	dat, err := json.Marshal(goodR)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-	w.WriteHeader(http.StatusOK)
-	w.Write(dat)
+	})
 }
