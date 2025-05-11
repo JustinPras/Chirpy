67ef361e090dec20101557c8d5381bd01e277862
JustinPras <justinponpras@gmail.com>
2025-05-11 21:06:54 +1000

    fix: user struct

--- handler_login.go
+++ handler_login.go
@@ -3,16 +3,18 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/JustinPras/Chirpy/internal/auth"
-	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Password 	string 	`json:"password`
 		Email 		string 	`json:"email"`
+	}
+
+	type response struct {
+		User
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -25,28 +27,22 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 
 	user, err := cfg.db.GetUserByEmail(r.Context(), params.Email)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Incorrect email", err)
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
 	err = auth.CheckPasswordHash(user.HashedPassword, params.Password)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Incorrect password", err)
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
-	type returnVals struct {
-		Id 			uuid.UUID 	`json:"id"`
-		CreatedAt 	time.Time 	`json:"created_at"`
-		UpdatedAt 	time.Time 	`json:"updated_at"`
-		Email 		string 		`json:"email"`
-	}
-
-
-	respondWithJSON(w, http.StatusOK, returnVals{
-		Id:			user.ID,
-		CreatedAt: 	user.CreatedAt,
-		UpdatedAt: 	user.UpdatedAt,
-		Email: 		user.Email,
+	respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID:	user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email: user.Email,
+		},
 	})
 }
--- handler_users_create.go
+++ handler_users_create.go
@@ -10,10 +10,22 @@ import (
 	"github.com/google/uuid"
 )
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+}
+
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Password 	string 	`json:"password`
 		Email 		string 	`json:"email"`
+	}
+
+	type response struct {
+		User
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -40,19 +52,13 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create new user", err)
 		return
 	}
-
-	type returnVals struct {
-		Id 			uuid.UUID 	`json:"id"`
-		CreatedAt 	time.Time 	`json:"created_at"`
-		UpdatedAt 	time.Time 	`json:"updated_at"`
-		Email 		string 		`json:"email"`
-	}
-
 	
-	respondWithJSON(w, http.StatusCreated, returnVals{
-		Id:	user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email: user.Email,
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:	user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email: user.Email,
+		},
 	})
 }
