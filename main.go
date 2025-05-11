e05ba1d95511e816aaadb2efb79f1177637fc8a7
JustinPras <justinponpras@gmail.com>
2025-05-11 16:27:46 +1000

    fix: chirps endpoint - add chirps

--- main.go
+++ main.go
@@ -44,8 +44,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerChirpsValidate)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 
 	server := &http.Server{
 		Addr: ":" + port,
--- validate.go
+++ validate.go
@@ -5,15 +5,17 @@ import (
 	"net/http"
 	"strings"
 	"slices"
+	"fmt"
+	"time"
+
+	"github.com/JustinPras/Chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		CleanedBody string `json:"cleaned_body"`
+		Body 	string 		`json:"body"`
+		UserId 	uuid.UUID	`json:"user_id"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -24,18 +26,50 @@ func (cfg *apiConfig) handlerChirpsValidate(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	const maxChirpLength = 140
-	if len(params.Body) > maxChirpLength {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+	chirpBody, err := validateChirp(params.Body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Chirp", err)
 		return
 	}
 
-	
-	censoredChirp := censorProfanity(params.Body)
+	chirpParams := database.CreateChirpParams {
+		Body: 	chirpBody,
+		UserID: params.UserId,
+	}
 
-	respondWithJSON(w, http.StatusOK, returnVals{
-		CleanedBody: censoredChirp,
+	chirp, err := cfg.db.CreateChirp(r.Context(), chirpParams)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create new chirp", err)
+		return
+	}
+
+
+	type returnVals struct {
+		ID 			uuid.UUID 	`json:"id"`
+		CreatedAt 	time.Time 	`json:"created_at"`
+		UpdatedAt 	time.Time 	`json:"updated_at"`
+		Body 		string 		`json:"body"`
+		UserID		uuid.UUID 	`json:"user_id"`
+	}
+
+	respondWithJSON(w, http.StatusCreated, returnVals{
+		ID:			chirp.ID,
+		CreatedAt: 	chirp.CreatedAt,
+		UpdatedAt: 	chirp.UpdatedAt,
+		Body: 		chirp.Body,
+		UserID:		chirp.UserID,
 	})
+
+}
+
+func validateChirp(chirp string) (string, error) {
+	const maxChirpLength = 140
+	if len(chirp) > maxChirpLength {
+		return "", fmt.Errorf("Chirp is too long")	
+	}
+
+	censoredChirp := censorProfanity(chirp)
+	return censoredChirp, nil
 }
 
 func censorProfanity(chirp string) string {
