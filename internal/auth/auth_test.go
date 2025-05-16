f5560fa3b85e695f64c99213959662c14d97a378
JustinPras <justinponpras@gmail.com>
2025-05-16 15:19:18 +1000

    feat: unit tests for JWTs

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestHashPassword(t *testing.T) {
@@ -17,5 +20,36 @@ func TestCheckPasswordHash(t *testing.T) {
 	err := CheckPasswordHash(hash, password)
 	if err != nil {
 		t.Errorf("Hash does not match password: %v", err)
+	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	userID, _ := uuid.Parse("6b61fa75-e753-4e95-bc5b-c8aa9c629e1a")
+	const expiresIn = 5 * time.Minute
+	const tokenSecret = "7Lqz!pD@f9$e3Gx^KbJ6Wu#cZrMqT1vN"
+	
+	_, err := MakeJWT(userID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("Error making JWT: %v", err)
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	userID, _ := uuid.Parse("6b61fa75-e753-4e95-bc5b-c8aa9c629e1a")
+	const expiresIn = 5 * time.Minute
+	const tokenSecret = "7Lqz!pD@f9$e3Gx^KbJ6Wu#cZrMqT1vN"
+	
+	tokenString, err := MakeJWT(userID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("Error making JWT: %v", err)
+	}
+
+	userIDActual, err := ValidateJWT(tokenString, tokenSecret)
+	if err != nil {
+		t.Errorf("Error validating JWT: %v", err)
+	}
+
+	if userID != userIDActual {
+		t.Errorf("Expecting: %v\nActual: %v", userID, userIDActual)
 	}
 }
